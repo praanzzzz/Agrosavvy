0eadf14a343d3122a821fe6878882542a3ef05d2
Franz Anthony Gabijan <franzgabijan55@gmail.com>
2024-11-18 16:12:01 +0800

    Add files via upload

--- project_agrosavvy/settings.py
+++ project_agrosavvy/settings.py
@@ -92,10 +92,21 @@ WSGI_APPLICATION = 'project_agrosavvy.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'agrosavvydb',
+        'USER': 'postgres',
+        'PASSWORD': 'NASA',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
