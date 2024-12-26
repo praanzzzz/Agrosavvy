41091a405cd0752e61fb2ed0725e0c4db6d24d78
Franz Anthony Gabijan <franzgabijan55@gmail.com>
2024-12-26 20:08:00 +0800

    Add files via upload

--- project_agrosavvy/settings.py
+++ project_agrosavvy/settings.py
@@ -145,12 +145,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 # serve static and media files thru this url
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # stores uploaded files in a static and media directory
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
